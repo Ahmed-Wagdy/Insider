f0dbee89af43258882e272aef45c7a499f9234b7
omar <omar.m.osama1989@gmail.com>
2016-06-05 21:44:19 +0200

    multiple keys for twitter API

--- frontend/insider/app/controllers/api_controller.rb
+++ frontend/insider/app/controllers/api_controller.rb
@@ -1,8 +1,11 @@
 class ApiController < ApplicationController
   def index
+  	# wikipedia API
 	require 'wikipedia'
-  	@page = Wikipedia.find( 'iphone' )
+	query = 'iphone'
+  	@page = Wikipedia.find( query )
 
+  	# amazon API
 	require 'vacuum'
 	request = Vacuum.new('UK')
 	request.configure(
@@ -12,7 +15,7 @@ class ApiController < ApplicationController
 	)
 	@response = request.item_search(
 	  query: {
-	    'Keywords' => 'sony',
+	    'Keywords' => query,
 	    'SearchIndex' => 'All'
 	  }
 	)
@@ -23,5 +26,20 @@ class ApiController < ApplicationController
 	  }
 	)
 	@item = @result.to_h['ItemLookupResponse']['Items']['Item']
+
+	# twitter API
+	client = Twitter::Streaming::Client.new do |config|
+	  config.consumer_key        = "uFnrltP2gGJ4Muyub303AHiL9"
+	  config.consumer_secret     = "VJxvTEG0iE3xNaeKs2iNuqsBLIhb728hWWnBe53Ebnmwe6tigY"
+	  config.access_token        = "739405916559249408-s2kaVDajJqO4e5NC3RLLrzz2p0n8o0k"
+	  config.access_token_secret = "7jgGcGjK5gz837v8rGoK2kryZqRX8wJArrv79CcvyYt4C"
+	end
+
+	client = Rails.cache.read("client")
+	topics = [query,query.gsub(' ','_'),query.gsub(' ','')]
+	client.filter(track: topics.join(",")) do |tweet|
+		obj = {'id' => tweet.id,'text' => tweet.text,'favorite_count' => tweet.favorite_count,'retweet_count' => tweet.retweet_count,'created_at' => tweet.created_at}
+		byebug
+	end
   end
 end
