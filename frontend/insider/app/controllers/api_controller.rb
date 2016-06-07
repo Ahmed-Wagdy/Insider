0c5a2b50a4e814f61450e4b5f2c3a6058f998088
omar <omar.m.osama1989@gmail.com>
2016-06-07 13:55:23 +0200

    separate actions

--- frontend/insider/app/controllers/api_controller.rb
+++ frontend/insider/app/controllers/api_controller.rb
@@ -1,43 +1,78 @@
 class ApiController < ApplicationController
   def index
-  	# wikipedia API
-	require 'wikipedia'
-	query = 'iphone'
-  	@page = Wikipedia.find( query )
+ #  	# wikipedia API
+	# require 'wikipedia'
+	# query = 'iphone'
+ #  	@page = Wikipedia.find( query )
 
-  	# amazon API
-	require 'vacuum'
-	request = Vacuum.new('UK')
-	request.configure(
-	    aws_access_key_id: 'AKIAIOFOSMSJUOFHJX5A',
-	    aws_secret_access_key: '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',
-	    associate_tag: 'tag'
-	)
-	@response = request.item_search(
-	  query: {
-	    'Keywords' => query,
-	    'SearchIndex' => 'All'
-	  }
-	)
-	asin = @response.to_h['ItemSearchResponse']['Items']['Item'][0]['ASIN']
-	@result = request.item_lookup(
-	  query: {
-	    'ItemId' => asin
-	  }
-	)
-	@item = @result.to_h['ItemLookupResponse']['Items']['Item']
+ #  	# amazon API
+	# require 'vacuum'
+	# request = Vacuum.new('UK')
+	# request.configure(
+	#     aws_access_key_id: 'AKIAIOFOSMSJUOFHJX5A',
+	#     aws_secret_access_key: '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',
+	#     associate_tag: 'tag'
+	# )
+	# @response = request.item_search(
+	#   query: {
+	#     'Keywords' => query,
+	#     'SearchIndex' => 'All'
+	#   }
+	# )
+	# asin = @response.to_h['ItemSearchResponse']['Items']['Item'][0]['ASIN']
+	# @result = request.item_lookup(
+	#   query: {
+	#     'ItemId' => asin
+	#   }
+	# )
+	# @item = @result.to_h['ItemLookupResponse']['Items']['Item']
+
+	# foursquare API
+	fs_client = Foursquare2::Client.new(:client_id => 'DFE2PJPNV0TVYZJ4ZPIUBHW4HQODKZPSYC5VOBC4BZWPZ5TK', :client_secret => 'IGACMN05NXCYRXTIMUAX0NOZZEUBKBWJLLMHYANXKBZEUHJH',:oauth_token => 'SNSENWFLDJNXMO54XDUOBIOVS4YRBX1RNAHCWJNQ4IVCDA2T',:api_version => '20140806')
+	Rails.cache.write("fs_client",fs_client)
 
 	# twitter API
-	client = Twitter::Streaming::Client.new do |config|
-	  config.consumer_key        = "uFnrltP2gGJ4Muyub303AHiL9"
-	  config.consumer_secret     = "VJxvTEG0iE3xNaeKs2iNuqsBLIhb728hWWnBe53Ebnmwe6tigY"
-	  config.access_token        = "739405916559249408-s2kaVDajJqO4e5NC3RLLrzz2p0n8o0k"
-	  config.access_token_secret = "7jgGcGjK5gz837v8rGoK2kryZqRX8wJArrv79CcvyYt4C"
+	twitter_keys_id = Rails.application.config.twitter_keys_itr
+	twitter_keys_count = Rails.application.config.twitter_keys_count
+	tkey = Tkey.find(twitter_keys_id)
+	twitter_keys_id = twitter_keys_id % twitter_keys_count + 1
+	Rails.application.config.twitter_keys_itr = twitter_keys_id
+	twitter_client = Twitter::Streaming::Client.new do |config|
+	  config.consumer_key        = tkey.consumer_key
+	  config.consumer_secret     = tkey.consumer_secret
+	  config.access_token        = tkey.access_token
+	  config.access_token_secret = tkey.access_token_secret
 	end
+	Rails.cache.write('twitter_client',twitter_client)
+  end
 
-	client = Rails.cache.read("client")
+  def searchList
+  	query = params[:query]
+  	type = params[:type]
+  	if type == 'place'
+  		require "net/http"
+		require "uri"
+		fs_client = Rails.cache.read("fs_client")
+		# byebug
+		# client_ip = request.remote_ip
+		# client_ip = '41.234.19.65'
+		client_ip = '81.21.107.92'
+		uri = URI.parse('http://freegeoip.net/json/'+client_ip)
+		response = Net::HTTP.get_response(uri)
+		location = JSON.parse(response.body)
+		geo = location["latitude"].to_s + ',' + location["latitude"].to_s
+		@places = fs_client.search_venues(:ll => geo, :query => query)
+		byebug
+  	elsif type == 'product'
+  	end
+  	
+  end
+
+  def tweetsAnalysis
+  	query = params[:query]
+  	twitter_client = Rails.cache.read("twitter_client")
 	topics = [query,query.gsub(' ','_'),query.gsub(' ','')]
-	client.filter(track: topics.join(",")) do |tweet|
+	twitter_client.filter(track: topics.join(",")) do |tweet|
 		obj = {'id' => tweet.id,'text' => tweet.text,'favorite_count' => tweet.favorite_count,'retweet_count' => tweet.retweet_count,'created_at' => tweet.created_at}
 		byebug
 	end
--- frontend/insider/config/routes.rb
+++ frontend/insider/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'api/index'
-
+  get 'api/searchList'
+  get 'api/tweetsAnalysis'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
